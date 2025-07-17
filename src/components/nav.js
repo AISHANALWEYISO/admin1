985d3919249673ba4107ebfe3bed315523f70e34
AISHANALWEYISO <nalweyisoa22@gmail.com>
2025-07-17 11:59:57 +0300

    Updated navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import '../styles/nav.css';
-import logo from '../images/logo.png'
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import '../styles/nav.css';
+// import logo from '../images/logo.png'
+// import { Link } from 'react-router-dom';
 
-function Navbar() {
-  return (
-    <nav className="navbar">
+// function Navbar() {
+//   return (
+//     <nav className="navbar">
       
-       <img
-          src={logo}
-          alt="Yucca Logo"     style={{ height: '40px', width: 'auto', marginRight: '100px' }}
-        />
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
-  );
-}
+//        <img
+//           src={logo}
+//           alt="Yucca Logo"     style={{ height: '40px', width: 'auto', marginRight: '100px' }}
+//         />
+//       <ul className="navbar-links">
+//         <li><Link to="/">Home</Link></li>
+//         <li><Link to="/about">About</Link></li>
+//         <li><a href="/services">Services</a></li>
+//         <li><a href="/contact">Contact</a></li>
+//       </ul>
+//     </nav>
+//   );
+// }
 
-export default Navbar;
+// export default Navbar;
+
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from './assets/logo.png'; // Adjust path as needed
+
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="light" expand="lg" sticky="top">
+      <Container>
+        {/* Logo and Brand */}
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          <img
+            src={logo}
+            alt="Logo"
+            width="40"
+            height="40"
+            className="d-inline-block align-top me-2"
+          />
+          <span className="fw-bold">MyWebsite</span>
+        </Navbar.Brand>
+
+        {/* Mobile Toggle */}
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+
+            <NavDropdown title="About" id="about-dropdown">
+              <NavDropdown.Item as={Link} to="/story">Our Story</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/team">Our Team</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/products">Products</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/partners">Our Partners</NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link as={Link} to="/services">Services</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
+
+
 
