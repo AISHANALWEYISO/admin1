1e0e2f2f9874e3a5038d6963b113f8b352fbd433
AISHANALWEYISO <AISHANALWEYISO>
2025-07-18 18:19:52 +0300

    added pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Nav from './components/nav';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/contact'
+import Story from './components/story'
+import Footer from './components/footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -14,8 +17,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+           <Route path="/story" element={<Story />} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
       </div>
+      <div> <Footer /> </div>
     </Router>
   );
 }
